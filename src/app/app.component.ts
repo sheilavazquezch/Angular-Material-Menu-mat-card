c69440ccfc01232266707d109b1e0654df9c19bc
sheilavazquezch <l17390367@chetumal.tecnm.mx>
2022-06-27 00:47:21 -0500

    commit inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
-
+import { UsuarioService} from './usuario.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ejercicioMatcard';
+
+  usuario:any;
+
+  constructor(private usuarioService: UsuarioService) {}  
+
+  ngOnInit() {
+    this.recuperarPersona();
+  }  
+
+  recargar() {
+    this.recuperarPersona();
+  }
+
+  recuperarPersona() {
+    this.usuarioService.retornar()
+      .subscribe( result =>  {this.usuario = result});    
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
